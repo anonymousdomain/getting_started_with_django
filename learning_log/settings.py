1a1eee1f7fd4cf8e3fd7658f121e26ba67919469
anonymousdomain <domainanonymous1@gmail.com>
2021-12-12 02:51:25 +0300

    custom error templates

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i2s*ii50hsy&=*%+=urr5rvr07y4pb63r0(!z(^0(n23%=innc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =False
 
 ALLOWED_HOSTS = []
 
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
