172326f2d30e22afa37736a5158ccf7e8e0de28b
anonymousdomain <domainanonymous1@gmail.com>
2021-12-12 03:02:03 +0300

    local setting variable changes

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
@@ -20,7 +20,8 @@ def topics(request):
 
 @login_required
 def topic(request, topic_id):
-    topic_ls = Topic.objects.get(id=topic_id)
+    #topic_ls = Topic.objects.get(id=topic_id)
+    topic_ls=get_object_or_404(Topic,id=topic_id)
     if topic_ls.owner != request.user:
         raise Http404
     entries = topic_ls.entry_set.order_by('-date_added')
